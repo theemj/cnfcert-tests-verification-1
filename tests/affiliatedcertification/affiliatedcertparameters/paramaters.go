9695420a4d2d16bfbac92358963ebe668375b088
Emily Johnson <emjohnso@redhat.com>
2021-12-23 16:01:11 -0500

    Automated first operator test and added relevant parameters

--- tests/affiliatedcertification/affiliatedcertparameters/paramaters.go
+++ tests/affiliatedcertification/affiliatedcertparameters/paramaters.go
@@ -8,4 +8,8 @@ var (
 	CertifiedContainerNodeJsUbi          = "nodejs-12/ubi8"
 	CertifiedContainerRhel7OpenJdk       = "openjdk-11-rhel7/openjdk"
 	UncertifiedContainerFooBar           = "foo/bar"
+	TestCaseOperatorAffiliatedCertName   = "affiliated-certification affiliated-certification-operator-is-certified"
+	CertifiedOperatorApicast             = "apicast-operator/redhat-operators"
+	CertifiedOperatorKubeturbo           = "kubeturbo-certified/certified-operators"
+	UncertifiedOperatorBarFoo            = "bar/foo"
 )
